a51bcc72e56b01b8110f70cd76335b0e0d264a7c
ianhein <ianhein50@gmail.com>
2024-01-14 12:09:14 -0300

    Update global CSS styles and package dependencies

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/footer";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <main className="pt-40 pb-20 bg-slate-100">
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
