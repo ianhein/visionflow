6275ef3c69784d66877e5bd9207c58239502804c
ianhein <ianhein50@gmail.com>
2024-01-16 14:48:08 -0300

    Refactor list header and add card functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,22 +1,25 @@
 "use client";
 
-import { useState, useRef, ElementRef } from "react";
+import { toast } from "sonner";
 import { List } from "@prisma/client";
-import { Toaster, toast } from "sonner";
 import { useEventListener } from "usehooks-ts";
+import { useState, useRef, ElementRef } from "react";
 
 import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
-
 import { FormInput } from "@/components/form/form-input";
+
+import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
+
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
 
@@ -24,6 +27,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
     setIsEditing(true);
     setTimeout(() => {
       inputRef.current?.focus();
+      inputRef.current?.select();
     });
   };
 
@@ -31,22 +35,9 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
     setIsEditing(false);
   };
 
-  const onBlur = () => {
-    formRef.current?.requestSubmit();
-  };
-
-  const onKeyDown = (e: KeyboardEvent) => {
-    if (e.key === "Escape") {
-      formRef.current?.requestSubmit();
-    }
-  };
-
-  useEventListener("keydown", onKeyDown);
-
   const { execute } = useAction(updateList, {
     onSuccess: (data) => {
       toast.success(`Renamed to "${data.title}"`);
-
       setTitle(data.title);
       disableEditing();
     },
@@ -71,33 +62,43 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
     });
   };
 
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+
   return (
-    <>
-      <Toaster richColors expand={false} />
-      <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start- gap-x-2">
-        {isEditing ? (
-          <form ref={formRef} action={handleSubmit} className="flex-1 px-[2px]">
-            <input hidden id="id" name="id" value={data.id} />
-            <input hidden id="boardId" name="boardId" value={data.boardId} />
-            <FormInput
-              ref={inputRef}
-              onBlur={onBlur}
-              id="title"
-              placeholder="Enter list title..."
-              defaultValue={title}
-              className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
-            />
-            <button type="submit" hidden />
-          </form>
-        ) : (
-          <div
-            onClick={enableEditing}
-            className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
-          >
-            {title}
-          </div>
-        )}
-      </div>
-    </>
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start- gap-x-2">
+      {isEditing ? (
+        <form ref={formRef} action={handleSubmit} className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={data.id} />
+          <input hidden id="boardId" name="boardId" value={data.boardId} />
+          <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
+            id="title"
+            placeholder="Enter list title.."
+            defaultValue={title}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+          <button type="submit" hidden />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
+      <ListOptions onAddCard={onAddCard} data={data} />
+    </div>
   );
 };
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+
 import { ListWithCards } from "@/types";
 import { ListHeader } from "./list-header";
+import { CardForm } from "./card-form";
+import { cn } from "@/lib/utils";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -9,10 +14,39 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ data, index }: ListItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const disableEditing = () => setIsEditing(false);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} />
+        <ListHeader data={data} onAddCard={enableEditing} />
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0"
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card}></CardItem>
+          ))}
+        </ol>
+        <CardForm
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          disableEditing={disableEditing}
+          enableEditing={enableEditing}
+        />
       </div>
     </li>
   );
