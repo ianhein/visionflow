5e1c54e0cc3d5291b4f2e6994196f1d8ca48e0e7
ianhein <ianhein50@gmail.com>
2024-01-17 02:16:50 -0300

    Add QueryProvider to PlatformLayout and useQuery in CardModal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,22 +1,27 @@
 "use client";
 
-import { CardWithList } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
 import { useCardModal } from "@/hooks/use-card-modal";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { CardWithList } from "@/types";
+import { fetcher } from "@/lib/fetcher";
+import { Header } from "./header";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
-        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
-          <div className="col-span-3">
-            <div className="w-full space-y-6">dasd</div>
-          </div>
-        </div>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
       </DialogContent>
     </Dialog>
   );
